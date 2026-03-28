2c08f30e60bdd96fc351dcb7dd07d6f6715aca1a
Keane Arazaan <kienarzn@gmail.com>
2026-03-28 20:05:58 +0700

    first commit

--- app/src/main/java/com/example/register/MainActivity.java
+++ app/src/main/java/com/example/register/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.register;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -16,16 +19,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        Spinner spinnerCity = findViewById(R.id.spinnerCity);
 
-        String[] cities = {"Bandung", "Jakarta", "Surabaya", "Yogyakarta", "Semarang"};
+        EditText etEmail = findViewById(R.id.etEmail);
+        EditText etUsername = findViewById(R.id.etUsername);
+        Spinner spinnerCity = findViewById(R.id.spinnerTier);
+        Button btnCreateAccount = findViewById(R.id.btnRegisterMember);
+
+        String[] tiers = {"Silver Member", "Gold Member", "Platinum Member"};
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_spinner_item, cities);
+                android.R.layout.simple_spinner_item, tiers);
 
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         spinnerCity.setAdapter(adapter);
+
+        btnCreateAccount.setOnClickListener(v -> {
+            String email = etEmail.getText().toString();
+            String username = etUsername.getText().toString();
+
+            if (email.isEmpty() || username.isEmpty()) {
+                Toast.makeText(MainActivity.this, "Please fill in all fields", Toast.LENGTH_SHORT).show();
+            } else {
+                // Simulasi berhasil register
+                Toast.makeText(MainActivity.this, "Registration Successful for " + username, Toast.LENGTH_LONG).show();
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
